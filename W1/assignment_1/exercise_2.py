2b6339f14cfc02fff479910650a35a90d40bcc1f
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-13 10:48:37 +0200

    make minor adjustments

--- W1/assignment_1/exercise_2.py
+++ W1/assignment_1/exercise_2.py
@@ -1,21 +1,25 @@
+# first n prime numbers
+
 def is_prime(num):
 	for i in range(2, num):
 		if (num % i) == 0:
 			return False
 	return True
 
-input_text = input("please give a sequence lenght: ")
+input_text = int(input("please give a sequence lenght: "))
 
-n = int(input_text)
+n = input_text
 i = 2
+len = 0
 prime_list = []
 
 while 1:
 	if is_prime(i):
 		prime_list.append(i)
-	if len(prime_list) == n:
+		len += 1
+	if len == n:
 		break
 	i += 1
 
-for i, n in enumerate(prime_list, 1):
-	print(f"{i}. {n}")
+for counter, num in enumerate(prime_list, start=1):
+	print(f"{counter}. {num}")
