58c55861c3c7d8ee865df68704cd817b1d499c20
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-22 14:18:57 +0200

    add better commenting, improve code

--- W2/assignment_2/exercise_3.py
+++ W2/assignment_2/exercise_3.py
@@ -1,23 +1,33 @@
 def symbol_to_value(c):
     """Return decimal value of Roman symbol
     
-    :param c: a char representing a roman symbol
+    :param c: char representing a Roman symbol
     :type c: str
 
     :rtype: int
-    :return: decimal value of Roman symbol if vaild symbol, else None
+    :return: decimal value of Roman symbol
+    
+    .. note::
+         If invaild symbol is passed, the function returns 0
     """
-    roman_symbols = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
+    roman_symbols = {
+        "I": 1, 
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    
     return roman_symbols[c] if c in roman_symbols else 0
 
-
-def roman_to_value(roman, ignore_sub_rule=False):
-    """Convert and return Roman number to decimal
+def roman_to_value(roman, ignore_sub_rule = False):
+    """Return Roman number to decimal number
     
     :param roman: sequence of roman symbols, representing a Roman number
     :type roman: string
-    :param ignore_sub_rule: defines if subtraction rule applies
+    :param ignore_sub_rule: defines if subtraction rule applies, defaults to False
     :type ignore_sub_rule: bool
     
     :return: decimal value of a Roman number
@@ -25,21 +35,24 @@ def roman_to_value(roman, ignore_sub_rule=False):
     """
     roman_decimal_value = 0
 
-    if ignore_sub_rule == True:
-        for symbol in roman:
-            roman_decimal_value += symbol_to_value(symbol)
+    if ignore_sub_rule:
+        return sum([symbol_to_value(sym) for sym in roman])
     else:
-        idx = 0
-        while idx < len(roman):
-            val_1 = symbol_to_value(roman[idx])
-            val_2 = (
-                symbol_to_value(roman[idx + 1]) if idx + 1 in range(len(roman)) else -1
-            )
-            if val_1 < val_2:
-                roman_decimal_value += val_2 - val_1
-                idx += 1
+        i = 0
+        while i < len(roman):
+            sym = {
+                "left": symbol_to_value(roman[i]),
+                "right": symbol_to_value(roman[i + 1])
+                if i + 1 in range(len(roman))
+                else -1,
+            }
+
+            # apply substraction rule if applicable
+            if sym["right"] > sym["left"]:
+                roman_decimal_value += sym["right"] - sym["left"]
+                i += 1
             else:
-                roman_decimal_value += val_1
-            idx += 1
+                roman_decimal_value += sym["left"]
+            i += 1
 
     return roman_decimal_value
