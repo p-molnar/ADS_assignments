b704d3654641f753fa81f265792cc2ad958d75e7
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-22 13:12:12 +0200

    improve docstring wording, add comment

--- W2/assignment_2/exercise_2.py
+++ W2/assignment_2/exercise_2.py
@@ -1,13 +1,17 @@
 def ln2_approximation(N):
-    """Return an approximation for natural logarithm of 2 for `N` terms
+    """Return an approximation for natural logarithm of 2
+        iterated over `N` terms
     
-    :param N: iteration count, defines the number of iteration for the approximation
+    :param N: iteration count, defines the number of terms of the series
     :type N: int
     
     :rtype: float
     :return: approximation of natural logarithm of 2
     """
     ln2_approximate_value = 0
+
+    # iterate the formula `N` times with counter of `k` starting from 1
     for k in range(1, N + 1):
         ln2_approximate_value += 1 / ((3 ** k) * k) + 1 / ((4 ** k) * k)
+
     return ln2_approximate_value
