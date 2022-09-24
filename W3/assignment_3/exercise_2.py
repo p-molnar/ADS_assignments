971a2317977ebcacf0b269c1282726fd4cafd25c
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-24 21:43:45 +0200

    finish exercise, to be checked

--- W3/assignment_3/exercise_2.py
+++ W3/assignment_3/exercise_2.py
@@ -1,21 +1,34 @@
 from data_loader import *
 from exercise_1 import *
 
+def filter_data(data, col_idx, condition):
+	data_selection = []
+
+	for criteria in condition:
+		if isinstance(criteria, list):	
+			for crit in criteria:
+				data_selection += list(filter(lambda row: row[col_idx] == crit, data))
+		elif isinstance(criteria, tuple):
+			lower_bound, upper_bound = criteria
+			data_selection += list(filter(lambda row: int(row[col_idx]) > lower_bound and int(row[col_idx]) < upper_bound, data))
+
+	return data_selection
+
 def get_group(data, headers, condition):	
 	n_row, n_col = len(data), len(headers)
 	if isinstance(condition, str) or isinstance(condition, int):
 		return get_by_axis(data, headers, condition)
-	elif isinstance(condition, dict): # do error checking!
-		ret_list = []
-		k, v = list(condition.keys()), list(condition.values())
 
-		for col in k:
-			col_idx = headers.index(col)
-			for row in data:
-				if row[col_idx] == v[0][0]:
-					ret_list += row
-		return ret_list
-	return None
+	elif isinstance(condition, dict): # do error checking!
+		data_selection = data.copy()
+
+		for k, v in condition.items():
+			col_idx = headers.index(k)
+			data_selection = filter_data(data_selection, col_idx, v)
+
+		return data_selection
 
 data, headers = load_data_from_csv("kwb-2019.csv")
-print(get_group(data, headers, {'region': ["Amsterdam"]}))
+# print(get_group(data, headers, {'region': []}))
+# print(get_group(data, headers, {'region': ["Amsterdam", "Utrecht"]}))
+print(get_group(data, headers, {'region': ["Rotterdam", "Utrecht"], 'population': (500000, 700000)}))
