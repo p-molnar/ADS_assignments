14f1afb9ad57ffa16eb4ce4d20dabf6ba512dafa
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-25 19:03:01 +0200

    bugfix

--- W3/assignment_3/exercise_2.py
+++ W3/assignment_3/exercise_2.py
@@ -4,20 +4,19 @@ from exercise_1 import *
 def filter_data(data, col_idx, condition):
 	data_selection = []
 
-	for criteria in condition:
-		if isinstance(criteria, list):	
-			for crit in criteria:
-				data_selection += list(filter(lambda row: row[col_idx] == crit, data))
-		elif isinstance(criteria, tuple):
-			lower_bound, upper_bound = criteria
-			data_selection += list(filter(lambda row: int(row[col_idx]) > lower_bound and int(row[col_idx]) < upper_bound, data))
+	if isinstance(condition, list):	
+		for criteria in condition:
+			data_selection += list(filter(lambda row: row[col_idx] == criteria, data))
+	elif isinstance(condition, tuple):
+		lower_bound, upper_bound = condition 
+		data_selection += list(filter(lambda row: int(row[col_idx]) > lower_bound and int(row[col_idx]) < upper_bound, data))
 
 	return data_selection
 
 def get_group(data, headers, condition):	
-	n_row, n_col = len(data), len(headers)
+
 	if isinstance(condition, str) or isinstance(condition, int):
-		return get_by_axis(data, headers, condition)
+		data_selection = get_by_axis(data, headers, condition)
 
 	elif isinstance(condition, dict): # do error checking!
 		data_selection = data.copy()
@@ -26,9 +25,9 @@ def get_group(data, headers, condition):
 			col_idx = headers.index(k)
 			data_selection = filter_data(data_selection, col_idx, v)
 
-		return data_selection
+	return data_selection
 
 data, headers = load_data_from_csv("kwb-2019.csv")
-# print(get_group(data, headers, {'region': []}))
+# print(get_group(data, headers, {'men': ["581", "17703"]}))
 # print(get_group(data, headers, {'region': ["Amsterdam", "Utrecht"]}))
-print(get_group(data, headers, {'region': ["Rotterdam", "Utrecht"], 'population': (500000, 700000)}))
+# print(get_group(data, headers, {'region': ["Rotterdam", "Utrecht"], 'population': (500000, 700000)})criteria)
