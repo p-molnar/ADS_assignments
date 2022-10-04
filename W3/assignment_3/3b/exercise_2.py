8dfcd3fe88eabd39d202f41ebdbc1c05db05a553
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-10-04 14:23:10 +0200

    fix recursion

--- W3/assignment_3/3b/exercise_2.py
+++ W3/assignment_3/3b/exercise_2.py
@@ -1,66 +1,72 @@
 from exercise_1 import *
 from city_map import *
 
-BUILDING = 0
 EMPTY = 1
-PARKING = -1
 
-direction = {"up": 10, "down": 20, "left": 30, "right": 40}
-
-print("START")
+direction = {
+	"up": 10,
+	"down": 20,
+	"left": 30,
+	"right": 40
+}
 
 def is_valid_coordinate(x, y, map_dimension):
 	return y in range(map_dimension["HEIGHT"]) and x in range(map_dimension["WIDTH"])
 
-def find_route(route_map, current_loc, visited_locs):
-
-	x, y = current_loc
+def path_finder_backtracker(map, start_loc, visited_locs):
+	x, y = start_loc
 	dim = {
-		"WIDTH": len(route_map[0]),
-		"HEIGHT": len(route_map),
+		"WIDTH": len(map[0]),
+		"HEIGHT": len(map),
 	}
-	
-	if x == dim["WIDTH"] - 1:
-		print("finish\n")
+	if not is_valid_coordinate(x, y, dim):
+		return 0
+
+	if x == dim["WIDTH"] - 1 and map[y][x] == EMPTY:
 		return 1
-	
-	visited_locs.append((x, y))
 
-	if is_valid_coordinate(x, y, dim) and route_map[y][x] == EMPTY:
-		
-		route_map[y][x] = direction["up"]
-		if find_route(route_map, (x, y - 1), visited_locs):
+	if is_valid_coordinate(x, y, dim) and map[y][x] == EMPTY:
+		# print(map)
+		visited_locs.append(start_loc)
+		map[y][x] = direction["up"]
+		if path_finder_backtracker(map, (x, y - 1), visited_locs):
 			return 1
-		route_map[y][x] = direction["down"]
-		if find_route(route_map, (x, y + 1), visited_locs):
+		map[y][x] = direction["down"]
+		if path_finder_backtracker(map, (x, y + 1), visited_locs):
 			return 1
-		route_map[y][x] = direction["left"]
-		if find_route(route_map, (x - 1, y), visited_locs):
+		map[y][x] = direction["left"]
+		if path_finder_backtracker(map, (x - 1, y), visited_locs):
 			return 1
-		route_map[y][x] = direction["right"]
-		if find_route(route_map, (x + 1, y), visited_locs):
+		map[y][x] = direction["right"]
+		if path_finder_backtracker(map, (x + 1, y), visited_locs):
 			return 1
-
-		route_map[y][x] = EMPTY
-	# if y - 1 in range(len(route_map)) and route_map[y - 1][x] == EMPTY and (y - 1, x) not in visited_locs:
-	# 	route_map[y][x] = direction["up"]
-	# 	find_route(route_map, (x, y - 1), visited_locs)
-	# elif x + 1 in range(len(route_map)) and route_map[y][x + 1] == EMPTY and (y, x + 1) not in visited_locs:
-	# 	route_map[y][x] = direction["right"]
-	# 	find_route(route_map, (x + 1, y), visited_locs)
-	# elif y + 1 in range(len(route_map)) and route_map[y + 1][x] == EMPTY and (y + 1, x) not in visited_locs:
-	# 	route_map[y][x] = direction["down"]
-	# 	find_route(route_map, (x, y + 1), visited_locs)
-	# elif x - 1 in range(len(route_map)) and route_map[y][x - 1] == EMPTY and (y, x - 1) not in visited_locs:
-	# 	route_map[y][x] = direction["left"]
-	# 	find_route(route_map, (x - 1, y), visited_locs)
-
-	# print(visited_locs)	
-	# print(route_map, "\n")
+		map[y][x] = EMPTY
 	return 0
 
+def find_route(route_map, current_loc, visited_locs):
+	solved_map = route_map.copy()
+
+	if path_finder_backtracker(solved_map, current_loc, visited_locs):
+		return solved_map
+	return None
+
+
+# city_map = np.array([
+#     [ 1, 1, 1, 1, 1 ],
+#     [ 1, 0, 1, 1, 1 ],
+#     [ 1, 1, 1, 1, 1 ],
+#     [ 1, 1, 1, 1, 0 ],
+#     [ 1, 1, 1, 0, 1 ]])
+
 marked_map = mark_parking_locations(city_map)
-# start_loc -> (x, y)
-# y -> row, depth
-# x -> col, width (?)
-print(find_route(marked_map, (0, 2), []))
+
+# marked_map = np.array([
+# 	[ 1, 1, 1, ],
+# 	[ 1, 0, 1, ],
+# 	[ 1, 0, 1, ]])
+print(marked_map)
+
+for i in range(12):
+	print(f"iter: (0, {i})")
+	print(find_route(marked_map, (0, i), []))
+
