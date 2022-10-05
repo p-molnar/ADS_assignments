e4e1ecaa7c3766ce9332318c42bc6a679e22cc00
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-10-05 22:40:25 +0200

    add docstring, format doc

--- W3/assignment_3/3b/exercise_2.py
+++ W3/assignment_3/3b/exercise_2.py
@@ -5,25 +5,37 @@ direction = {"up": 10, "down": 20, "left": 30, "right": 40}
 
 
 def is_valid_coordinate(x, y, map_dimension):
+    """Logical test whether a given pair of coordinates are within the boundaries of the map
+
+    :param x: x coordinate
+    :type x: int
+    :param y: y coordinate
+    :type y: int
+    :param map_dimension: a dictionary storing the dimensions of the map
+    :type map_dimension: dict
+
+    :rtype: Bool
+    :return: True if the pair of coordinate is within the boundaries of the map, else False 
+    """
     return y in range(map_dimension["HEIGHT"]) and x in range(map_dimension["WIDTH"])
 
 
 def path_finder_backtracker(map, start_loc, visited_locs):
     """The function recursively finds and marks the first solution
-	of the map
+    of the map
 
-	:param map: the map on which the direction is to be marked
-	:type map: numpy.ndarray
-	:param start_loc: the starting position from where the map is to be solved
-	:type start_loc: tuple
-	:param visited_locs: a list that collects locations the backtracker visited
-	:type visited_locs: list
+    :param map: the map on which the direction is to be marked
+    :type map: numpy.ndarray
+    :param start_loc: the starting position from where the map is to be solved
+    :type start_loc: tuple
+    :param visited_locs: a list that collects locations the backtracker visited
+    :type visited_locs: list
 
-	:rtype: Bool
-	:return: returns True to the main caller, and to the recursive caller 
-	either if the map is solved or a given direction is valid in a given 
-	`start_loc`, 0 if the direction is invalid, or the map has no solution
-	"""
+    :rtype: Bool
+    :return: returns True to the main caller, and to the recursive caller 
+    either if the map is solved or a given direction is valid in a given 
+    `start_loc`, 0 if the direction is invalid, or the map has no solution
+    """
     x, y = start_loc
     dim = {
         "WIDTH": len(map[0]),
@@ -54,16 +66,16 @@ def path_finder_backtracker(map, start_loc, visited_locs):
 def find_route(route_map, current_loc, visited_locs):
     """Finds and returns a solution for the map passed to it
 
-	:param route_map: the map on which the direction is to be marked
-	:type map: numpy.ndarray
-	:param start_loc: the starting position from where the map is to be solved
-	:type start_loc: tuple
-	:param visited_locs: a list that collects locations that the backtracker visited
-	:type visited_locs: list
+    :param route_map: the map on which the direction is to be marked
+    :type map: numpy.ndarray
+    :param start_loc: the starting position from where the map is to be solved
+    :type start_loc: tuple
+    :param visited_locs: a list that collects locations that the backtracker visited
+    :type visited_locs: list
 
-	:rtype: numpy.ndarray|None
-	:return: if the map has a solution, it returns a potential solution, otherwise nothing
-	"""
+    :rtype: numpy.ndarray|None
+    :return: if the map has a solution, it returns a potential solution, otherwise nothing
+    """
     # duplicate map to avoid overriding the original map
     solved_map = route_map.copy()
 
